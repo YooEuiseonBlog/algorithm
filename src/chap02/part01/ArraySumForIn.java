8c65ae65b55ac6d57a055c9ee64691d4d363acc5
esy <yesman5307@gmail.com>
2022-12-15 15:05:07 +0900

    finshed The algorithm for Array

---
+++ src/chap02/part01/ArraySumForIn.java
@@ -0,0 +1,7 @@
+package chap02.part01;
+//배열의 모든 요소의 합을 구하여 출력함(확장 for문)
+public class ArraySumForIn {
+    public static void main(String[] args) {
+        double[] a = {1.0, 2.0, 3.0, 4.0, 5.0};
+    }
+}
